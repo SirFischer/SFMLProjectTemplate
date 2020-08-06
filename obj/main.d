66c2f36f2820ce646f28024859fb5b591705b9a7
Marek <marek5886@hotmail.com>
2020-08-06 21:26:00 +0200

    cleanup

--- obj/main.d
+++
@@ -1,8 +0,0 @@
-obj/main.o: srcs/main.cpp includes/Core.hpp includes/System/Window.hpp \
- mfGUI/includes/mfGUI.hpp mfGUI/includes/widgets/Widget.hpp \
- mfGUI/includes/utils/Events.hpp \
- mfGUI/includes/widgetComponents/Background.hpp \
- mfGUI/includes/widgetComponents/EventManager.hpp \
- mfGUI/includes/widgetComponents/Text.hpp \
- mfGUI/includes/widgets/Image.hpp mfGUI/includes/widgets/Button.hpp \
- mfGUI/includes/widgets/Text.hpp
